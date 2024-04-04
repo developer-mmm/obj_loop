515a43930d422e3244e90bb393132a2bce4ff0a0
SmartSystem <mrozimatov9@gmail.com>
2024-04-04 22:25:48 +0500

    loop in object

---
+++ index.js
@@ -0,0 +1,89 @@
+//7 - masala;
+
+// function fn(obj) {
+//     for (let key in obj) {
+//         console.log(key);
+//     }
+// }
+// const student = {
+//     name: "David Rayy",
+//     sclass: "VI",
+//     rollno: 12
+// }
+// fn(student)
+
+////////////////////////////////////////////////
+
+// 8 - masala;
+
+// const student = {
+//     name: "David Rayy",
+//     class: "VI",
+//     rollno: 12
+// };
+// delete student.rollno;
+// console.log(student);
+
+////////////////////////////////////////////////////
+
+// 9 - masala;
+
+// function students(fn){
+//     return Object.keys(fn).length
+// }
+// const student2 = {
+// 	name : "David Rayy",
+// 	sclass : "VI",
+// 	rollno : 12,
+// 	age: 23,
+// 	address: 'world'
+// }
+
+// console.log(students(student2));
+
+////////////////////////////////////////////////
+
+// 10 - masala;
+
+// function Person(name, earn, expenses) {
+//     this.name = name;
+//     this.earn = earn;
+//     this.expenses = expenses;
+//     this.getMonthlyIncome = function () {
+//         return this.earn - this.expenses;
+//     }
+// }
+// let person1 = new Person("John", 1000, 200);
+// console.log(`${person1.name}ning harajatddan tashqari puli  $${person1.getMonthlyIncome()}`)
+// let person2 = new Person("Tom", 1500, 300);
+// console.log(`${person2.name}ning harajatddan tashqari puli  $${person2.getMonthlyIncome()}`)
+// let person3 = new Person("Jek", 2000, 100);
+// console.log(`${person3.name}ning harajatddan tashqari puli  $${person3.getMonthlyIncome()}`)
+// let person4 = new Person("Sara", 5000, 2450);
+// console.log(`${person4.name}ning harajatddan tashqari puli  $${person4.getMonthlyIncome()}`)
+// let person5 = new Person("Bell", 2500, 700);
+// console.log(`${person5.name}ning harajatddan tashqari puli  $${person5.getMonthlyIncome()}`)
+// let person6 = new Person("Mask", 10000, 4000);
+// console.log(`${person6.name}ning harajatddan tashqari puli  $${person6.getMonthlyIncome()}`)
+
+////////////////////////////////////////////////////////////////////////////////
+
+// 11 - masala;
+
+// const car = {
+//     rusumi: 'nexia 3',
+//     rangi: 'Mokry',
+//     start() {
+//         console.log('🟡 vrum vrum');
+//     },
+//     drive() {
+//         console.log('🟢 driving');
+//     },
+//     stop() {
+//         console.log('🛑 stoping');
+//     }
+// }
+
+// car.drive()
+
+////////////////////////////////////////////////////////////////////////////
